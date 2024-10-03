006ab3ab9e45717e3186d0d8ff5a20bd1b53f0d1
hlong7TWU <138091719+hlong7TWU@users.noreply.github.com>
2024-10-02 20:25:10 -0500

    For starting point

---
+++ src/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='bg-white '>
+        <h1 className='text-[12px]'>Hello</h1>
+    </div>
+  )
+}
+
+export default App
+
