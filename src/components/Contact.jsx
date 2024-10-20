e3dadb1523d56aad0ec92d828b0dcd3678369f6c
hlong7TWU <138091719+hlong7TWU@users.noreply.github.com>
2024-10-20 14:04:04 -0500

    Adding in the Contact page

---
+++ src/components/Contact.jsx
@@ -0,0 +1,65 @@
+import React, { useState } from "react";
+
+const Contact = () => {
+    const[formData, setFormData] = useState({
+        name: '',
+        email: '',
+        message: '',
+
+
+    });
+
+    const handleChange = (e) =>{
+        e.preventDefault();
+        console.log('Form submit:', formData);
+        setFormData({name: '', email:'', message:''});
+    };
+
+    return(
+        <div>
+<h1> Contact us here</h1>
+<form onSubmit={handleSubmit}>
+                <div>
+                    <label>
+                        Name:
+                        <input
+                            type="text"
+                            name="name"
+                            value={formData.name}
+                            onChange={handleChange}
+                            required
+                        />
+                    </label>
+                </div>
+                <div>
+                    <label>
+                        Email:
+                        <input
+                            type="email"
+                            name="email"
+                            value={formData.email}
+                            onChange={handleChange}
+                            required
+                        />
+                    </label>
+                </div>
+                <div>
+                    <label>
+                        Message:
+                        <textarea
+                            name="message"
+                            value={formData.message}
+                            onChange={handleChange}
+                            required
+                        />
+                    </label>
+                </div>
+                <button type="submit">Send Message</button>
+            </form>
+        </div>
+    );
+};
+
+export default Contact;
+
+
