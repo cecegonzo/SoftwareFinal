e229199aebe2cfbbe99b4cc4eb3365284d98fd60
hlong7TWU <138091719+hlong7TWU@users.noreply.github.com>
2024-11-10 11:19:17 -0600

    Update Contact.jsx

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -26,7 +26,7 @@ const Contact =() => {
 
         <div className="input-box">
           <label> Your Question?</label>
-          <textarea name="" id="" classname="field message" placeholder='Enter your question' required ></input>
+          <textarea name="" id="" classname="field message" placeholder='Enter your question' required ></textarea>
         </div>
 
         <button type="submit"> Send Message </button>
