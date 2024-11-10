3f311112244f3ccaf68b7e07efebfa12857cc880
hlong7TWU <138091719+hlong7TWU@users.noreply.github.com>
2024-11-10 13:28:14 -0600

    starting the service page

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
-
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
 const Services = () => {
   return (
+    <div className='bg-white'>
+      <Navbar />
     <div>
       
     </div>
+  
+
+<Footer />
+</div>
   )
 }
 
